09179750a69a49ce28bf401c32d8f59a74043ed5
Jose Luis <jl_mj_17@hotmail.com>
2014-07-15 01:38:18 -0500

    Documentación de LoadGraph
    Se añadió la documentación del modulo LoadGraph

--- LoadGraph.hs
+++ LoadGraph.hs
@@ -15,24 +15,23 @@ loadFile filename = do
                 contents <- readFile filename
                 return (lines contents)
 
-
-
+-- Divide la lista de Strings por el separador "|"				
 splitString :: [String] ->[[String]]
 splitString xs =[ splitOn "|" a |  a <- xs]
 
-
-
+-- Convierte una lista de Strings en una lista de triplas
 transformToTripla :: [[String]] -> [(String,String,String)]
 transformToTripla y= [(a,b,c) | [a,b,c] <- y]
 
 
-
+-- Convierte el ultimo valor String de cada una de las triplas a un valor Int
 convertLastToInt :: [(String,String,String)] -> [(String,String,Int)]
 convertLastToInt z= [(a,b, read c :: Int) | (a,b,c) <- z]
-
 
 -- Se envia como parametro la lista de lineas del archivo y devolverá una lista de triplas
 finalGraph :: [String] -> [(String,String,Int)]
 finalGraph a =  convertLastToInt (transformToTripla (splitString a))
+
+
 
 
