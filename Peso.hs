03ea2189a7be1b7896037d151163314ec880652a
Jose Luis <jl_mj_17@hotmail.com>
2014-07-20 23:04:09 -0500

    Modificación modulo Peso

--- Peso.hs
+++ Peso.hs
@@ -1,13 +1,21 @@
+module Peso
+	( convertSolutionToTuples,
+	  obtainRoutesWithWeight,
+	  pesoTotal
+	) where
 
+-- Función que permite convertir lista de Strings en una lista de tuplas
 convertSolutionToTuples :: [String] -> [(String,String)] 
 convertSolutionToTuples [] = []
 convertSolutionToTuples [x] = []
 convertSolutionToTuples (x:y:xs) = (x,y):(convertSolutionToTuples (y:xs))
 
 
-ObtainRoutesWithDistance :: [(String,String)] -> [(String,String,Int)] -> [(String,String,Int)]
-ObtainRoutesWithDistance x y= [ (r,s,t) | (a,b) <- x, (r,s,t) <- y, a==r && b==s ]
+-- Función que permitirá agregar los pesos a las rutas obtenidas por una solución de dijkstra
+obtainRoutesWithWeight :: [(String,String)] -> [(String,String,Int)] -> [(String,String,Int)]
+obtainRoutesWithWeight x y = [ (r,s,t) | (a,b) <- x, (r,s,t) <- y, a==r && b==s ]
 
 
+-- Función para obtener el peso total de la ruta mas corta obtenida por una solución de dijkstra
 pesoTotal :: [(String,String,Int)] -> Int
 pesoTotal xs = sum [ c | (a,b,c) <- xs]
